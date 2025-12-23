73e4f824a7da1a48984f0d30ac38e5b0eb2f9feb
Adise <admin@pauibanez.me>
2025-12-23 23:15:06 +0100

    added SDL3 dependencies and build command

--- build.bat
+++ build.bat
@@ -1,10 +1,12 @@
 @echo off
 if not exist build mkdir build
 if not exist build\objects mkdir build\objects
+if not exist build\sdl3.dll cp lib\SDL3\SDL3.dll build\sdl3.dll
 
 setlocal enabledelayedexpansion
 
 call node ./build.js
+
 endlocal
 
 if errorlevel 1 (
