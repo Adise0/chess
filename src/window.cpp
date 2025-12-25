d2bf8892d045111a5c173e1ee58a1907129c404b
Nil <nilbadia@gmail.com>
2025-12-25 01:57:15 +0100

    dona error perque li surt dels ous

--- src/window.cpp
+++ src/window.cpp
@@ -10,7 +10,7 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     return 0;
   }
 
-  return DefWindowProc(hWnd, uMsg, wParam, lParam)
+  return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
 
