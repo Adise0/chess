2af9ee3ac3a267cb381b1f4f4213ffe0663d338c
Nil <nilbadia@gmail.com>
2025-12-30 21:02:02 +0100

    added main maneu components

--- src/ui/screens/mainMenu/MainMenu.h
+++ src/ui/screens/mainMenu/MainMenu.h
@@ -1,9 +1,16 @@
-
-
+#include "../../elements/button/Button.h"
+#include "../../elements/rectangle/Rectangle.h"
 
 namespace Chess::Rendering::Screens {
+using namespace Elements;
 
 class MainMenu {
+
 private:
+  /// @brief The background image
+  Rectangle background;
+
+  /// @brief The play button
+  Button playButton;
 };
 } // namespace Chess::Rendering::Screens
